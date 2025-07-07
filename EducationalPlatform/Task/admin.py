b3323adfcca4685dfeb0904bfbf46e3f52badf97
AMMAR-OM <134091512+AMMAR-OM@users.noreply.github.com>
2025-07-07 23:59:28 +0300

    Add files via upload

---
+++ EducationalPlatform/Task/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import Enrollment, CustomUser, Course ,Profile ,Assignment, Grade, Payment, Message, Complaint, Report, Quiz
+admin.site.register(Enrollment)
+admin.site.register(CustomUser)
+admin.site.register(Course)
+admin.site.register(Profile)
+admin.site.register(Assignment)
+admin.site.register(Grade)
+admin.site.register(Payment)
+admin.site.register(Message)
+admin.site.register(Complaint)
+admin.site.register(Report)
+admin.site.register(Quiz)
+
